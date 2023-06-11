071ed26b140cafc91ab36b768963f6567d437d3b
etherbits <nika.qvrivishviliPC@Gmail.com>
2023-06-11 15:07:14 +0400

    adding add button

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,30 +1,73 @@
-import { StyleSheet } from "react-native";
+import { Pressable, StatusBar, StyleSheet } from "react-native";
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
 
 import { Text, View } from "react-native";
+import Colors from "../../constants/Colors";
 
 export default function TabOneScreen() {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Home</Text>
-      <Text style={styles.body}>Hello from android</Text>
+      <Text style={styles.title}>Workouts</Text>
+      <View style={styles.boxList}>
+        <View style={styles.box}>
+          <Text style={styles.boxText}>Upper Body Workout</Text>
+        </View>
+      </View>
+      <Pressable style={styles.addButton}>
+        <FontAwesome5 size={24} color={Colors.gray[50]} name="plus" />
+      </Pressable>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    position: "relative",
     flex: 1,
+    alignItems: "flex-start",
+    justifyContent: "flex-start",
+    paddingTop: StatusBar.currentHeight || 0 + 16,
+    padding: 16,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: "bold",
+    color: Colors.black,
+    textShadowColor: Colors.gray[50],
+    textShadowRadius: 1,
+  },
+  body: {
+    fontSize: 18,
+    color: Colors.gray[50],
+  },
+  boxList: {
+    width: "100%",
+    padding: 20,
+    gap: 20,
+  },
+  box: {
+    width: "100%",
+    height: 160,
+    backgroundColor: Colors.gray[900],
+    borderRadius: 16,
     alignItems: "center",
     justifyContent: "center",
   },
-  title: {
+  boxText: {
+    color: Colors.slate[50],
     fontSize: 20,
-    fontWeight: "bold",
   },
-  body: { fontSize: 18 },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: "80%",
+  addButton: {
+    position: "absolute",
+    bottom: 0,
+    right: 0,
+    marginRight: 32,
+    marginBottom: 16,
+    backgroundColor: Colors.red[500],
+    width: 48,
+    height: 48,
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 8,
   },
 });
